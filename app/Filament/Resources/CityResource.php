c73e97f176adbba7075d18551ae9b2e750088889
Kenneth Solomon <mr.kennethsolomon@gmail.com>
2022-12-16 00:40:33 +0800

    recorder navigation

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -21,6 +21,8 @@ class CityResource extends Resource
 
   protected static ?string $navigationIcon = 'heroicon-o-office-building';
   protected static ?string $navigationGroup = 'System Management';
+  protected static ?int $navigationSort = 2;
+
 
 
   public static function form(Form $form): Form
--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -20,6 +20,7 @@ class DepartmentResource extends Resource
 
   protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
   protected static ?string $navigationGroup = 'System Management';
+  protected static ?int $navigationSort = 2;
 
   public static function form(Form $form): Form
   {
