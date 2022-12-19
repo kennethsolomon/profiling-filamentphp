5a4bfa518e6074bc53fa84981aa3b1dfe22fcce8
Kenneth Solomon <mr.kennethsolomon@gmail.com>
2022-12-19 22:38:31 +0800

    redirect to index

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -10,6 +10,11 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getActions(): array
     {
         return [
